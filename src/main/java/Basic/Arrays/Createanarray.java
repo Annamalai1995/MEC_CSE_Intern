35ae72eac0b4465272fc9ad5e87a3f44c6de06e8
Annamalai1995 <s.annamalai1995@gmail.com>
2024-07-04 12:13:19 +0530

    Array

---
+++ src/main/java/Basic/Arrays/Createanarray.java
@@ -0,0 +1,11 @@
+package Basic.Arrays;
+
+import java.util.Arrays;
+
+public class Createanarray
+{
+    public static void main(String[] args) {
+        String [] names={"Gowthami","Annamalai"};
+        System.out.println(Arrays.toString(names));
+    }
+}
